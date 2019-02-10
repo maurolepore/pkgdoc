21f364945d81a0c4fd0c7dc65a33d2239274a9b9
Mauro Lepore <maurolepore@gmail.com>
2019-02-09 22:04:29 -0500

    More rigurous travis

--- R/imports.R
+++ R/imports.R
@@ -1,6 +1,7 @@
 #' @importFrom rlang .data %||% enquos set_names inform warn abort
 #' @importFrom dplyr filter select mutate group_by ungroup arrange
 #' @importFrom glue glue
+#' @importFrom utils isS3method
 NULL
 
 globalVariables(".")
