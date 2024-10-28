da1537264085d3e79c0ce2d7de85dbd676f57c78
Mauro Lepore <maurolepore@gmail.com>
2024-10-28 13:19:45 +0000

    Polish tests (#48)
    * Polish tests
    
    * style
    
    * Polish

--- tests/testthat/test-reference.R
+++ tests/testthat/test-reference.R
@@ -1,56 +1,51 @@
-test_that("adds a url", {
-  out <- reference_package("base", url = "https://blah")$topic[[1]]
-  expect_true(grepl("href", out))
-  expect_true(grepl("blah", out))
-})
-
-test_that("warns if a package isn't attached", {
-  reference_package("dplyr") |>
-    expect_warning("should be attached")
-})
-
 test_that("errors if the package does not exist", {
-  reference_package("badpackage") |>
-    expect_error("badpackage") |>
+  expect_error(reference_package("bad"), "bad") |>
     # Not ideal but also wans that the package isn't attached.
     expect_warning("should be attached")
 })
 
-test_that("is sensitive to strip_s3class", {
-  pull_all_equal <- function(x) {
-    x %>%
-      dplyr::filter(topic == "all.equal") %>%
-      dplyr::distinct() %>%
-      dplyr::pull(alias) %>%
-      strsplit(", ") %>%
-      unlist()
-  }
+test_that("yields the expected data.frame", {
+  out <- reference_package(c("datasets"))
 
-  strip_false <- reference_package("base", strip_s3class = FALSE) %>%
-    pull_all_equal()
-  expect_true("all.equal.character" %in% strip_false)
-
-  strip_true <- reference_package("base", strip_s3class = TRUE) %>%
-    pull_all_equal()
-  expect_false("all.equal.character" %in% strip_true)
+  expect_s3_class(out, "data.frame")
+  expect_named(out, c("topic", "alias", "title", "concept", "package"))
+  are_type <- unlist(unique(lapply(out, typeof)))
+  expect_equal(are_type, "character")
 })
 
-test_that("with fgeo outputs the expected data structure", {
-  skip_if_not_installed("fgeo")
-  withr::local_package("fgeo")
-
-  result <- reference_package("fgeo")
-  expect_s3_class(result, "data.frame")
-  expect_named(result, c("topic", "alias", "title", "concept", "package"))
-
-  expect_false(grepl("href", result$topic[[1]]))
+test_that("works with multiple packages", {
+  out <- reference_package(c("datasets", "grDevices"))
+  expect_equal(unique(out$package), c("datasets", "grDevices"))
 })
 
-test_that("no longer includes package documentation", {
-  skip_if_not_installed("fgeo")
-  withr::local_package("fgeo")
+test_that("warns if a package isn't attached", {
+  packages <- c("tools", "datasets")
+  expect_warning(reference_package(packages), "Not attached.*tools")
+})
 
-  expect_false(any(grepl("fgeo-package", unique(reference_package("fgeo")$alias))))
+test_that("with no url creates no link", {
+  out <- reference_package("datasets")$topic[[1]]
+  expect_false(grepl("href", out))
+})
+
+test_that("with a url creates a link", {
+  out <- reference_package("datasets", url = "https://blah")$topic[[1]]
+  expect_true(grepl("href", out))
+})
+
+test_that("by default srips teh class of S3 methods", {
+  out <- reference_package("base")
+  alias <- unique(out[grepl("all.equal", out$alias), "alias"])[[1]]
+  expect_false(grepl("numeric", alias))
+
+  out <- reference_package("base", strip_s3class = FALSE)
+  alias <- unique(out[grepl("all.equal", out$alias), "alias"])[[1]]
+  expect_true(grepl("numeric", alias))
+})
+
+test_that("doesn't include the package-level documentation", {
+  out <- reference_package("pkgdoc")
+  expect_false(any(grepl("pkgdoc-package", unique(out$alias))))
 })
 
 test_that("reference_concept() errors if some concept is not matched", {
